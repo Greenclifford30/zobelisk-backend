96f5d80480827140a41e29b1b8a5f0aef6f0dca2
Chris <chris.dopuch@gmail.com>
2014-03-20 12:20:00 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZobeliskBackend::Application.config.secret_key_base = 'c2bcf5c7f6fb5dbf662a5236b933e89d0c8b0fc6fadd0704026ae83f2ed4e497d8007a5ef4c8c6ec7a35eca59657fad99cfdcd4d4b7c3a69cf9f9b1ca0549e97'
