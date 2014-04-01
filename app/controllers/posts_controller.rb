3fc85c0b2cc4b6922aeed041d41183077614320b
Chris <chris.dopuch@gmail.com>
2014-04-01 13:43:08 -0500

    fixed typo with user_id on posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-    @post.user = current_user.id
+    @post.user_id = current_user.id
 
     respond_to do |format|
       if @post.save
