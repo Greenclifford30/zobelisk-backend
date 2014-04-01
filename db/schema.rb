77547935c2d3f71c36e394029f10edfbd73425e7
Chris <chris.dopuch@gmail.com>
2014-04-01 13:29:02 -0500

    adding migration for user_id in posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321020423) do
+ActiveRecord::Schema.define(version: 20140401182600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20140321020423) do
     t.string   "media"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "tags", force: true do |t|
