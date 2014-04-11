06a7113e361e379e5427c49e191df245a5673a63
Chris <chris.dopuch@gmail.com>
2014-04-10 20:24:53 -0500

    added tagged method, and tagged view

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,15 @@ ZobeliskBackend::Application.routes.draw do
 
   resources :posts
 
-#resources :users  
-devise_for :users
+  #resources :users  
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+
+  match 'tagged' => 'posts#tagged', :as => 'tagged'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
