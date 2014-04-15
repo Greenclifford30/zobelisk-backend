968b447a8c7681646a526cc97d30c0d6a247abdf
Chris <chris.dopuch@gmail.com>
2014-04-14 20:57:43 -0500

    adding beacon id to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -84,6 +84,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:email, :timestamp, :likes, :title, :event_date, :body_text, :media, :tag_list)
+      params.require(:post).permit(:email, :timestamp, :likes, :title, :event_date, :body_text, :media, :tag_list, :beacon_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408031825) do
+ActiveRecord::Schema.define(version: 20140415014318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,10 @@ ActiveRecord::Schema.define(version: 20140408031825) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "beacon_id"
   end
+
+  add_index "posts", ["beacon_id"], name: "index_posts_on_beacon_id", using: :btree
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
