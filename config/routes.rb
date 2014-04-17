d39c3f7457e985102e50081c183f1b93f97c1328
Seth <sjw3kf@mail.missouri.edu>
2014-04-17 12:39:26 -0500

    registration

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ ZobeliskBackend::Application.routes.draw do
   resources :posts
 
   #resources :users  
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
