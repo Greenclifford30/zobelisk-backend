6fb74dbf2a808d2f5e5db4ce49ac36b01365ff32
Chris <chris.dopuch@gmail.com>
2014-04-29 11:50:44 -0500

    added field to new user form for image upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417183259) do
+ActiveRecord::Schema.define(version: 20140429164626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,10 @@ ActiveRecord::Schema.define(version: 20140417183259) do
     t.string   "twitter"
     t.string   "picture"
     t.string   "authentication_token"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
