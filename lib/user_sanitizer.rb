06b3ff221ea430c7369fb4659b86420133cc112e
Chris <chris.dopuch@gmail.com>
2014-05-01 18:56:29 -0500

    adding the avatar fields to be allowed by sanitizer

--- lib/user_sanitizer.rb
+++ lib/user_sanitizer.rb
@@ -1,6 +1,20 @@
 class User::ParameterSanitizer < Devise::ParameterSanitizer
     private
     def account_update
-        default_params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :dob, :phone, :address, :twitter, :picture)
+        default_params.permit(	:first_name,
+        						:last_name,
+        						:email,
+        						:password,
+        						:password_confirmation,
+        						:current_password,
+        						:dob,
+        						:phone,
+        						:address,
+        						:twitter,
+        						:picture,
+        						:avatar_file_name,
+        						:avatar_content_type,
+        						:avatar_file_size,
+        						:avatar_updated_at )
     end
 end
