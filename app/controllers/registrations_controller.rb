690adcf09d5da4543a15ea707f94949a029f2614
Chris <chris.dopuch@gmail.com>
2014-05-01 13:16:49 -0500

    nevermind, the problem is with devise. reverting to Seth's code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,21 +5,10 @@ class RegistrationsController < Devise::RegistrationsController
     def create
 
         @user = User.new(user_params)
-=begin
         if @user.save
             render :json => { :state => { :code => 0 }, :data => @user }
         else
             render :json => { :state => { :code => 1, :messages => @user.errors.full_messages } }
-        end
-=end
-        respond_to do |format|
-              if @user.save
-                format.html { redirect_to @user, notice: 'User was successfully created.' }
-                format.json { render action: 'show', status: :created, location: @user }
-              else
-                format.html { render action: 'new' }
-                format.json { render json: @user.errors, status: :unprocessable_entity }
-              end
         end
     end
 
