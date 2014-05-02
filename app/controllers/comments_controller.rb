88e5ee8ce39e897ca81974b99e357df0517ecef8
Chris <chris.dopuch@gmail.com>
2014-05-01 20:32:24 -0500

    added post id to permitted params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -73,6 +73,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:email, :timestamp, :text_body)
+      params.require(:comment).permit(:email, :timestamp, :text_body, :post_id)
     end
 end
