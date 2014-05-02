d166ab9c881352546611681f7d6cd8cc73bea0ca
Chris <chris.dopuch@gmail.com>
2014-05-01 19:24:55 -0500

    ok moved the config statement around

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-config.gem "rmagick",
-:lib => "RMagick" 
+
 
 # Initialize the Rails application.
 ZobeliskBackend::Application.initialize!
+config.gem "rmagick",
+:lib => "RMagick" 
