1d7122d8e0968549ce59b546848d934dbf202dd1
Chris <chris.dopuch@gmail.com>
2014-05-12 18:36:12 -0500

    rebuilding posts

---
+++ db/migrate/20140512233401_create_posts_again.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :email
+      t.timestamp :timestamp
+      t.integer :likes
+      t.string :title
+      t.date :event_date
+      t.string :body_text
+      t.string :media
+      t.integer :beacon_id
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
