34c781c34e6b89800cd11c5af31409fce8c3c75d
Chris <chris.dopuch@gmail.com>
2014-05-13 00:26:44 -0500

    removed current user from comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,7 +29,7 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-    @comment.user_id = current_user.id
+    #@comment.user_id = current_user.id
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
